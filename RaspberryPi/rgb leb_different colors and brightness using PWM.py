e2f210ae4d36fb37b2bc877cec7170becc45ec30
Sai Meghana <51068183+saimeghana-y@users.noreply.github.com>
2022-07-21 22:06:51 +0530

    Update rgb leb_different colors and brightness using PWM.py

--- RaspberryPi/rgb leb_different colors and brightness using PWM.py
+++ RaspberryPi/rgb leb_different colors and brightness using PWM.py
@@ -1,42 +1,24 @@
-import RPi.GPIO as g
 import time
-g.setmode(g.BCM)
-g.setwarnings(False)
-ground = 10
-red = 11
-green = 12
-blue = 13
-g.setup(red, g.OUT)
-g.setup(green, g.OUT)
-g.setup(blue, g.OUT)
-g.setup(ground, g.OUT)
-g.OUTPUT(ground, g.LOW)
-p = g.PWM(11, 100)
-q = g.PWM(12, 100)
-r = g.PWM(13, 100)
+import RPi.GPIO as IO
+IO.setwarnings(False)
+IO.setmode(IO.BOARD)
+IO.setup(23,IO.OUT)
+IO.setup(32,IO.OUT)
+IO.setup(33,IO.OUT)
+p=IO.PWM(23,100)
 p.start(0)
-q.start(0)
-r.start(0)
-while True:
-    color = input()
-    if color == 'red':
-        for x in range(100):
-            p.ChangeDutyCycle(x)
-            time.sleep(0.05)
-        for x in range(100):
-            p.ChangeDutyCycle(100 - x)
-            time.sleep(0.05)
-    elif color == 'green':
-        for x in range(100):
-            q.ChangeDutyCycle(x)
-            time.sleep(0.05)
-        for x in range(100):
-            q.ChangeDutyCycle(100 - x)
-            time.sleep(0.05)
-    elif color == 'blue':
-        for x in range(100):
-            r.ChangeDutyCycle(x)
-            time.sleep(0.05)
-        for x in range(100):
-            r.ChangeDutyCycle(100 - x)
-            time.sleep(0.05)
+q=IO.PWM(32,100)
+r=IO.PWM(33,100)
+q.start(100)
+r.start(50)
+while 1:
+    for x in range(50):
+        p.ChangeDutyCycle(x)
+        q.ChangeDutyCycle(x)
+        r.ChangeDutyCycle(x)
+        time.sleep(1)
+    for x in range(50):
+        p.ChangeDutyCycle(50-x)
+        q.ChangeDutyCycle(50-x)
+        r.ChangeDutyCycle(50-x)
+        time.sleep(1)
