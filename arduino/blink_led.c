570f6f5a6bc8a6362f19fa56be2426317e3e57e4
Sai Meghana <51068183+saimeghana-y@users.noreply.github.com>
2022-07-21 19:08:50 +0530

    Create blink_led.c

---
+++ arduino/blink_led.c
@@ -0,0 +1,12 @@
+void setup(){
+pinMode(8,OUTPUT);
+pinMode(9,OUTPUT);
+}
+void loop(){
+digitalWrite(8,1);
+digitalWrite(9,0);
+delay(1000);
+digitalWrite(8,0);
+digitalWrite(9,1);
+delay(1000);
+}
