51b3093fe8a08bde90d943228c2bdc3da930ed4a
Sai Meghana <51068183+saimeghana-y@users.noreply.github.com>
2022-07-22 00:48:45 +0530

    Update rgb leb_different colors and brightness using PWM.py

--- RaspberryPi/rgb leb_different colors and brightness using PWM.py
+++ RaspberryPi/rgb leb_different colors and brightness using PWM.py
@@ -3,13 +3,13 @@ import RPi.GPIO as IO
 IO.setwarnings(False)
 IO.setmode(IO.BOARD)
 
-IO.setup(23,IO.OUT)
-IO.setup(32,IO.OUT)
-IO.setup(33,IO.OUT)
+IO.setup(10,IO.OUT)
+IO.setup(11,IO.OUT)
+IO.setup(12,IO.OUT)
 
-p=IO.PWM(23,100)
-q=IO.PWM(32,100)
-r=IO.PWM(33,100)
+p=IO.PWM(10,100)
+q=IO.PWM(11,100)
+r=IO.PWM(12,100)
 
 p.start(0)
 q.start(100)
