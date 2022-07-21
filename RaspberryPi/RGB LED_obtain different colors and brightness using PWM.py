9346455fffb9fb8a19afef6ac87febf103dfa832
Sai Meghana <51068183+saimeghana-y@users.noreply.github.com>
2022-07-21 19:26:59 +0530

    Update RGB LED_obtain different colors and brightness using PWM.py

--- RaspberryPi/RGB LED_obtain different colors and brightness using PWM.py
+++ RaspberryPi/RGB LED_obtain different colors and brightness using PWM.py
@@ -11,9 +11,9 @@ g.setup(green, g.OUT)
 g.setup(blue, g.OUT)
 g.setup(ground, g.OUT)
 g.OUTPUT(ground, g.LOW)
-p = g.PWM(11,100)
-q = g.PWM(12,100)
-r = g.PWM(13,100)
+p = g.PWM(11, 100)
+q = g.PWM(12, 100)
+r = g.PWM(13, 100)
 p.start(0)
 q.start(0)
 r.start(0)
@@ -24,19 +24,19 @@ while True:
             p.ChangeDutyCycle(x)
             time.sleep(0.05)
         for x in range(100):
-            p.ChangeDutyCycle(100-x)
+            p.ChangeDutyCycle(100 - x)
             time.sleep(0.05)
-    elif color =='green':
-         for x in range(100):
-             q.ChangeDutyCycle(x)
-             time.sleep(0.05)
-         for x in range(100):
-             q.ChangeDutyCycle(100-x)
-             time.sleep(0.05)
+    elif color == 'green':
+        for x in range(100):
+            q.ChangeDutyCycle(x)
+            time.sleep(0.05)
+        for x in range(100):
+            q.ChangeDutyCycle(100 - x)
+            time.sleep(0.05)
     elif color == 'blue':
         for x in range(100):
             r.ChangeDutyCycle(x)
             time.sleep(0.05)
         for x in range(100):
-            r.ChangeDutyCycle(100-x)
+            r.ChangeDutyCycle(100 - x)
             time.sleep(0.05)
